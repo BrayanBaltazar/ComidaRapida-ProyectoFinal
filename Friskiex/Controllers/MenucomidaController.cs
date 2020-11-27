ade2b76d1f08d5e9beb02748e49a7096ef15ec25
Q812 <udr97@hotmail.com>
2020-11-26 17:43:03 -0800

    Menú

---
+++ Friskiex/Controllers/MenucomidaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Friskiex.Controllers
+{
+    public class MenucomidaController : Controller
+    {
+        // GET: Menucomida
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
